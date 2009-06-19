d2571b2f31ee7e2e1b3b7110af49455f3bd3b271
Nick Carter <thynctank@thynctank.com>
2009-06-19 06:53:15 -0400

    very sketchy beginnings of lib classes, scene contents/interactions. Transaction needs to know which account to go back to

--- app/assistants/transaction-assistant.js
+++ app/assistants/transaction-assistant.js
@@ -1,11 +1,11 @@
-function AccountAssistant() {
+function TransactionAssistant() {
 	/* this is the creator function for your scene assistant object. It will be passed all the 
 	   additional parameters (after the scene name) that were passed to pushScene. The reference
 	   to the scene controller (this.controller) has not be established yet, so any initialization
 	   that needs the scene controller should be done in the setup function below. */
 }
 
-AccountAssistant.prototype.setup = function() {
+TransactionAssistant.prototype.setup = function() {
 	/* this function is for setup tasks that have to happen when the scene is first created */
 		
 	/* use Mojo.View.render to render view templates and add them to the scene, if needed. */
@@ -14,25 +14,22 @@ AccountAssistant.prototype.setup = function() {
 	this.controller.setupWidget("backButton", {}, {});
 	/* add event handlers to listen to events from widgets */
 	this.controller.listen("backButton", Mojo.Event.tap, function() {
-	  this.controller.stageController.swapScene("dashboard");
+	  this.controller.stageController.swapScene("account");
 	}.bindAsEventListener(this));
 };
 
-AccountAssistant.prototype.activate = function(event) {
+TransactionAssistant.prototype.activate = function(event) {
 	/* put in event handlers here that should only be in effect when this scene is active. For
 	   example, key handlers that are observing the document */
- 	$$(".transaction").invoke("observe", "click", function(evt) {
-     this.controller.stageController.swapScene("transaction");
-   }.bind(this), this);
 };
 
 
-AccountAssistant.prototype.deactivate = function(event) {
+TransactionAssistant.prototype.deactivate = function(event) {
 	/* remove any event handlers you added in activate and do any other cleanup that should happen before
 	   this scene is popped or another scene is pushed on top */
 };
 
-AccountAssistant.prototype.cleanup = function(event) {
+TransactionAssistant.prototype.cleanup = function(event) {
 	/* this function should do any cleanup needed before the scene is destroyed as 
 	   a result of being popped off the scene stack */
 };
