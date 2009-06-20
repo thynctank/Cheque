b175eeb66f380990c4e86fd3056c91a397317840
Nick Carter <thynctank@thynctank.com>
2009-06-20 18:20:18 -0400

    more sketching, including the beginnings of a platform-specific storage mechanism and an additional Hash class. Sketch of schemas for accounts, entries tables. Index currently includes scripts, remove them prior to production

---
+++ platform_storage.js
@@ -0,0 +1,6 @@
+var PlatformStorage = {
+  read: function(sql, success, failure) {},
+  write: function(sql, success, failure) {},
+  erase: function(sql, success, failure) {},
+  transact: function(func, success, failure) {}
+};
