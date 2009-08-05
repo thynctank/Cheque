73a0b6b30778306d241e6af7582b609dfca74046
Nick Carter <thynctank@thynctank.com>
2009-08-05 05:10:21 -0400

    account adding/deleting works

--- app/assistants/dashboard-assistant.js
+++ app/assistants/dashboard-assistant.js
@@ -1,6 +1,7 @@
 function DashboardAssistant() {
 	/* passed all the additional parameters (after the scene name) that were passed to pushScene. Reference
 	   to the scene controller (this.controller) has not be established yet */
+	window.asst = this;
 }
 
 DashboardAssistant.prototype.setup = function() {
@@ -18,7 +19,10 @@ DashboardAssistant.prototype.setup = function() {
 	this.controller.setupWidget("accountList", this.accountListAttributes, this.accountListModel);
 
 	this.controller.listen("accountList", Mojo.Event.listAdd, function(event) {
-	  this.controller.stageController.pushScene("new-account", checkbook);
+	  this.controller.showDialog({
+	    template: "dashboard/new-account-dialog",
+	    assistant: new AccountDialogAssistant(this)
+	  });
 	}.bind(this));
 
 	this.controller.listen("accountList", Mojo.Event.listDelete, function(event) {
@@ -45,3 +49,39 @@ DashboardAssistant.prototype.cleanup = function(event) {
 	/* this function should do any cleanup needed before the scene is destroyed as 
 	   a result of being popped off the scene stack */
 };
+
+var AccountDialogAssistant = Class.create({
+  initialize: function(sceneAssistant) {
+    this.sceneAssistant = sceneAssistant;
+    this.controller = sceneAssistant.controller;
+  },
+  setup: function(widget) {
+    this.widget = widget;
+    this.controller.setupWidget("newAccountName", {}, {
+      value: ""
+    });
+    
+    this.controller.setupWidget("newAccountBalance", {}, {
+      value: ""
+    });
+    
+    this.controller.setupWidget("saveAccountButton", {}, {buttonLabel: "Save"});
+  },
+  activate: function() {
+    this.controller.listen("saveAccountButton", Mojo.Event.tap, function(event) {
+      var options = {};
+      if(this.controller.get("newAccountName").mojo.getValue()) {
+        options.name = this.controller.get("newAccountName").mojo.getValue();
+        if(this.controller.get("newAccountBalance").mojo.getValue())
+          options.balance = this.controller.get("newAccountBalance").mojo.getValue();
+        checkbook.addOrAccessAccount(options, function() {
+          this.sceneAssistant.accountListModel.items = checkbook.accounts.getValues();
+          this.sceneAssistant.controller.modelChanged(this.sceneAssistant.accountListModel);
+          this.widget.mojo.close();
+        }.bind(this));
+      }
+      else
+        this.widget.mojo.close();
+    }.bind(this));
+  }
+});
