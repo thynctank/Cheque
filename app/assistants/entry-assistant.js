a0344be9a0bfd159073eb1a361a5484f9a213e97
Nick Carter <thynctank@thynctank.com>
2009-09-15 08:14:20 -0400

    entry scene fairly pretty now

--- app/assistants/entry-assistant.js
+++ app/assistants/entry-assistant.js
@@ -17,10 +17,14 @@ EntryAssistant.prototype.setup = function() {
   this.controller.setupWidget("category", {label: "Category"}, this.categoryModel = {choices: [], value: this.entry.category || ""});
   this.controller.setupWidget("subject", {}, this.subjectModel = {value: this.entry.subject || ""});
   this.controller.setupWidget("amount", {focus: true, charsAllow: positiveNumericOnly, modifierState: Mojo.Widget.numLock}, this.amountModel = {value: this.entry.amount ? this.entry.amount.toFinancialString() : ""});
+  this.controller.setupWidget("cleared", {choices: [
+      {label: "Pending", value: "0"},
+      {label: "Cleared", value: "1"}
+    ]}, this.clearedModel = {value: this.entry.cleared || "0"});
   this.controller.setupWidget("memo", {}, this.memoModel = {value: this.entry.memo || ""});
-  this.controller.setupWidget("cleared", {trueValue: "1", falseValue: "0"}, this.checkBoxModel = {value: this.entry.cleared || "0"});
   this.controller.setupWidget("date", {labelPlacement: Mojo.Widget.labelPlacementRight}, this.dateModel = {date: this.entry.date ? new Date(this.entry.date) : new Date()});
   this.controller.setupWidget("save", {type: Mojo.Widget.activityButton}, {buttonLabel: "Save"});
+  this.controller.setupWidget("detailsDrawer", {}, {open: false});
 
 	var entryState;
 	if(this.entry.id) {
@@ -46,13 +50,27 @@ EntryAssistant.prototype.setup = function() {
 	  this.entry.amount = this.controller.get("amount").mojo.getValue().toCents();
 	  this.entry.date = this.dateModel.date.getTime();
 	  this.entry.memo = this.memoModel.value;
-	  this.entry.cleared = parseInt(this.checkBoxModel.value, 10);
+	  this.entry.cleared = parseInt(this.clearedModel.value, 10);
 	  this.account.writeEntry(this.entry, function() {
 	    this.controller.stageController.popScene();
 	  }.bind(this));
 	}.bind(this);
 	
+	this.handleAddlDetailsToggle = function() {
+	  var arrow = this.controller.get("addlDetailsArrow");
+	  if(arrow.hasClassName("palm-arrow-closed")) {
+	    arrow.addClassName("palm-arrow-expanded");
+	    arrow.removeClassName("palm-arrow-closed");
+	  }
+	  else {
+	    arrow.addClassName("palm-arrow-closed");
+	    arrow.removeClassName("palm-arrow-expanded");
+	  }
+	  this.controller.get("detailsDrawer").mojo.toggleState();
+	}.bind(this);
+	
 	this.controller.listen("save", Mojo.Event.tap, this.handleSave);
+	$("addlDetails").observe("click", this.handleAddlDetailsToggle);
 };
 
 EntryAssistant.prototype.activate = function(event) {
@@ -82,4 +100,5 @@ EntryAssistant.prototype.cleanup = function(event) {
 	/* this function should do any cleanup needed before the scene is destroyed as 
 	   a result of being popped off the scene stack */
 	this.controller.stopListening("save", Mojo.Event.tap, this.handleSave);
+	$("addlDetails").stopObserving("click", this.handleAddlDetailsToggle);
 };
